7ff3559c0eac94afa63df088e7aa55fa38e700d6
David Zavala PC-DITSI <david.zavalal@udem.edu>
2024-03-08 12:58:54 -0600

    Base Project with Base Client

---
+++ Practica/Models/Doc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practica.Models
+{
+    public class Doc
+    {
+        [Key]
+        public int Id { get; set; } = -1;
+        [Required]
+        public string Name { get; set; } = "";
+        [Required]
+        public string Email { get; set; } = "";
+        [Required]
+        public string BirthDate { get; set; } = "";
+        [Required]
+        public string Education { get; set; } = "";
+        [Required]
+        public string DocPath { get; set; } = "";
+    }
+}
