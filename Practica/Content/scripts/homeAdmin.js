6f06db408af60e6cf13508b2327b37f714849d02
David Zavala PC-DITSI <david.zavalal@udem.edu>
2024-05-02 11:33:49 -0600

    better Datatables

--- Practica/Content/scripts/homeAdmin.js
+++ Practica/Content/scripts/homeAdmin.js
@@ -1,5 +1,86 @@
 $(document).ready(function () {
-    $('#DocsTable').DataTable();
+    $('#DocsTable').DataTable({
+        layout: {
+            top2Start: {
+                pageLength: {
+                    menu: [ 10, 25, 50, 100]
+                }
+            },
+            top2End: {
+                search: {
+                    placeholder: 'Busqueda general'
+                }
+            },
+            topStart: {
+                buttons: [
+                    {
+                        extend: 'pdfHtml5',
+                        text: 'PDF',
+                        exportOptions: {
+                            modifier: {
+                                page: 'current'
+                            },
+                            columns: [0,1,2,3,4,5,6,7]
+                        }
+                    },
+                    {
+                        extend: 'excelHtml5',
+                        text: 'Excel',
+                        exportOptions: {
+                            modifier: {
+                                page: 'current'
+                            },
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                        }
+                    }
+                ]
+            },
+            topEnd: null,
+            bottomStart: 'info',
+            bottomEnd: 'paging'
+        },
+        language: {
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        initComplete: function () {
+            $('#DocsTable tfoot tr').insertAfter($('#DocsTable thead'))
+            this.api()
+                .columns()
+                .every(function () {
+                    let column = this;
+                    let title = column.footer().textContent;
+
+                    let input = document.createElement('input');
+                    input.classList.add('form-control','form-control-sm','searchForColumn');
+                    input.placeholder = title;
+                    column.footer().replaceChildren(input);
+
+                    input.addEventListener('keyup', () => {
+                        if (column.search() != this.value) {
+                            column.search(input.value).draw();
+                        }
+                    });
+                });
+        }
+    });
+
     $('.ButtonViewJPG').on('click', function () {
         var filePath = $(this).val();
         var fileId = $(this).attr('fileId');
